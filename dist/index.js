2fa4d47871bd9e0373af530a79d18719cec3e0d9
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-09 22:01:46 +0900

    enum

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
 let id = 5;
+console.log("ID: ", id);
+// enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["up"] = 10] = "up";
+    Direction[Direction["down"] = 11] = "down";
+    Direction[Direction["right"] = 12] = "right";
+    Direction[Direction["left"] = 13] = "left";
+})(Direction || (Direction = {}));
+console.log(Direction.down);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 let id: number = 5
+
+console.log("ID: ", id);
+
+// enum
+enum Direction {
+  up = 10,
+  down,
+  right,
+  left
+}
+
+console.log(Direction.down);
